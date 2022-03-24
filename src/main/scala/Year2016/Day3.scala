0c299d59fe5402669a0423120990f44f7662ccef
Michael Kerry <michael.kerry@ovoenergy.com>
2022-03-24 16:49:57 +0000

    add 2016 day 3

---
+++ src/main/scala/Year2016/Day3.scala
@@ -0,0 +1,28 @@
+package Year2016
+
+import scala.io.Source.fromResource
+
+object Day3 extends App {
+  val input = fromResource("Year2016/Day3.txt")
+    .getLines()
+    .toList
+    .map(_.trim)
+    .map(_.split("\\s+").toList.map(_.toInt))
+
+  println(input)
+
+  def validateTriangle(sides: List[Int]): Boolean =
+    if ((sides(0) + sides(1)) > sides(2) && (sides(0) + sides(2)) > sides(1) && (sides(1) + sides(2)) > sides(0)) true else false
+
+  val resultPart1 = input.count(validateTriangle)
+  println(resultPart1)
+
+  val resultPart2 = input
+    .transpose
+    .map(_.grouped(3).toList)
+    .map(l => l.count(validateTriangle))
+    .sum
+
+  println(resultPart2)
+
+}
