d2de4ef9b0c2f76f28fb326e10311426c490347a
Michael Kerry <michael.kerry@ovoenergy.com>
2022-04-04 19:41:56 +0100

    Day5 part1 done, part2 wip

---
+++ src/main/scala/Year2016/Day5.scala
@@ -0,0 +1,45 @@
+package Year2016
+
+import java.security.MessageDigest
+import scala.annotation.tailrec
+
+object Day5 extends App {
+
+  def convertBytesToHex(bytes: Seq[Byte]): String = {
+    val sb = new StringBuilder
+    for (b <- bytes) {
+      sb.append(String.format("%02x", Byte.box(b)))
+    }
+    sb.toString
+  }
+
+  def convertToMd5Hex(input: String): String = {
+    val md = MessageDigest.getInstance("MD5")
+    val digest: Array[Byte] = md.digest(input.getBytes)
+    convertBytesToHex(digest)
+  }
+
+  @tailrec
+  def runChessPart1(input: String, password: String, digit: Int): String = {
+    val md5Hex = convertToMd5Hex(input + digit.toString)
+    val newPassword = if (md5Hex.startsWith("00000")) password + md5Hex.charAt(5) else password
+    if (newPassword.length == 8) newPassword
+    else runChessPart1(input, newPassword, digit + 1)
+  }
+
+//  val part1Answer = runChessPart1("reyedfim", "", 0)
+//  println(part1Answer)
+
+  @tailrec
+  def runChessPart2(input: String, passwordMap: Map[Int, Char], digit: Int): String = {
+    val md5Hex = convertToMd5Hex(input + digit.toString)
+    val newPassword = if (md5Hex.startsWith("00000") && !passwordMap.contains(md5Hex.charAt(5).toInt)) passwordMap ++ Map(md5Hex.charAt(5).toInt -> md5Hex.charAt(6)) else passwordMap
+    if (newPassword.keys.toList.length == 8) newPassword.toList.sortBy(_._1).map(_._2).mkString
+    else runChessPart2(input, newPassword, digit + 1)
+  }
+
+  val part2Answer = runChessPart2("abc", Map.empty[Int, Char], 0)
+  println(part2Answer)
+//  returns 05ce3f99 :(
+
+}
