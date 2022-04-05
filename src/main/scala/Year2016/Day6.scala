d0f382e1fd8778fa7f4bb691b1f8fbbb550d5599
Michael Kerry <michael.kerry@ovoenergy.com>
2022-04-05 15:40:44 +0100

    solved day 6

---
+++ src/main/scala/Year2016/Day6.scala
@@ -0,0 +1,26 @@
+package Year2016
+
+import scala.io.Source.fromResource
+
+object Day6 extends App {
+  val input = fromResource("Year2016/Day6.txt")
+    .getLines()
+    .toList
+    .map(_.toList)
+
+  val part1Answer = input
+    .transpose
+    .map(_.groupBy(identity).view.mapValues(_.length).toList.sortBy(_._2).reverse.head)
+    .map(_._1)
+    .mkString
+
+  println(part1Answer)
+
+  val part2Answer = input
+    .transpose
+    .map(_.groupBy(identity).view.mapValues(_.length).toList.minBy(_._2))
+    .map(_._1)
+    .mkString
+
+  println(part2Answer)
+}
