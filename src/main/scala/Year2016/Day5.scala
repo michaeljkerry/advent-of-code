da7a5f4691bc3127fb08ede6c5b1e885e7cdda9d
Michael Kerry <michael.kerry@ovoenergy.com>
2022-04-05 12:11:19 +0100

    solved day 5

--- src/main/scala/Year2016/Day5.scala
+++ src/main/scala/Year2016/Day5.scala
@@ -27,19 +27,20 @@ object Day5 extends App {
     else runChessPart1(input, newPassword, digit + 1)
   }
 
-//  val part1Answer = runChessPart1("reyedfim", "", 0)
-//  println(part1Answer)
+  val part1Answer = runChessPart1("reyedfim", "", 0)
+  println(part1Answer)
 
   @tailrec
   def runChessPart2(input: String, passwordMap: Map[Int, Char], digit: Int): String = {
     val md5Hex = convertToMd5Hex(input + digit.toString)
-    val newPassword = if (md5Hex.startsWith("00000") && !passwordMap.contains(md5Hex.charAt(5).toInt)) passwordMap ++ Map(md5Hex.charAt(5).toInt -> md5Hex.charAt(6)) else passwordMap
+    val newPassword = if (md5Hex.startsWith("00000") && md5Hex.charAt(5).isDigit && md5Hex.charAt(5).toString.toInt < 8 && !passwordMap.contains(md5Hex.charAt(5).toString.toInt))
+      passwordMap ++ Map(md5Hex.charAt(5).toString.toInt -> md5Hex.charAt(6))
+    else passwordMap
     if (newPassword.keys.toList.length == 8) newPassword.toList.sortBy(_._1).map(_._2).mkString
     else runChessPart2(input, newPassword, digit + 1)
   }
 
-  val part2Answer = runChessPart2("abc", Map.empty[Int, Char], 0)
+  val part2Answer = runChessPart2("reyedfim", Map.empty[Int, Char], 0)
   println(part2Answer)
-//  returns 05ce3f99 :(
 
 }
